57358806b33c2093b8b2b19bac49566d93fc21e5
Tsesang Dolkar <tsesang125@gmail.com>
2021-06-08 09:49:10 +0200

    added live weather icon

--- src/script.js
+++ src/script.js
@@ -29,17 +29,21 @@ function displayTemperature ( response ) {
     let humidityElement = document.querySelector( "#humidity" );
     let windElement = document.querySelector( "#wind" );
     let dateElement = document.querySelector( "#date" );
+    let iconElement = document.querySelector( "#icon" );
 
     temperatureElement.innerHTML = Math.round( response.data.main.temp );
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[ 0 ].description;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round( response.data.wind.speed );
-    dateElement.innerHTML = formatDate(response.data.dt*1000);
+    dateElement.innerHTML = formatDate( response.data.dt * 1000 );
+    iconElement.setAttribute(
+        "src", `http://openweathermap.org/img/wn/${ response.data.weather[ 0 ].icon }@2x.png` );
+    iconElement.setAttribute( "alt", response.data.weather[ 0 ].description );
 }
 
-let apiKey = "e7c5e1dd39ac83a06eb5ffdabc93a484";
-let city = "Switzerland";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiKey = "e7c5e1dd39ac83a06eb5ffdabc93a484";
+    let city = "Switzerland";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-axios.get( apiUrl ).then( displayTemperature );
+    axios.get( apiUrl ).then( displayTemperature );
